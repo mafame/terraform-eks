331106bc3ceb67c43a5bb24e41b1640edfd39797
amelieolga <88943611+amelieolga@users.noreply.github.com>
2021-11-18 22:09:29 -0500

    Update providers.tf

--- EKS/providers.tf
+++ EKS/providers.tf
@@ -1,6 +1,14 @@
 #
 # Provider Configuration
-#
+terraform {
+required_version="~> 1.0" 
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
 
 provider "aws" {
   region  = "us-west-1"
