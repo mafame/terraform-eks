1a6f4053a7c5e6dcf9b3e6af30b1eee69b9b3c7a
amelieolga <88943611+amelieolga@users.noreply.github.com>
2021-11-18 23:20:23 -0500

    Update vpc.tf

--- EKS/vpc.tf
+++ EKS/vpc.tf
@@ -24,10 +24,10 @@ resource "aws_subnet" "demo" {
   map_public_ip_on_launch = true
   vpc_id                  = aws_vpc.demo.id
 
-  tags = tomap ({
-    Name= "terraform-eks-demo-node"
-    (kubernetes.io/cluster/${var.cluster-name}) ="shared" 
-    })
+  #tags = tomap ({
+  #  Name= "terraform-eks-demo-node"
+   # (kubernetes.io/cluster/${var.cluster-name}) ="shared" 
+   # })
 }
 
 resource "aws_internet_gateway" "demo" {
