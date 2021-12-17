f63c4ec213817eb5f0dff3f973e025e087e0b699
amelieolga <88943611+amelieolga@users.noreply.github.com>
2021-12-17 10:54:55 -0500

    Update instances.tf

--- EKS/instances.tf
+++ EKS/instances.tf
@@ -1,1 +1,13 @@
-
+# 2 Instance Of Redhat Servers(t2.medium)
+resource "aws_instance" "redhat_servers"{
+  count                  = 4
+  ami                    = var.redhatami
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.demo_sg.id]
+  subnet_id              = element(aws_subnet.subnets.*.id, count.index)
+  key_name               = var.key_name
+  user_data              = file("init_script.sh")
+  tags = {
+    Name = "Redhat_Server_${count.index + 1}"
+  }
+}
